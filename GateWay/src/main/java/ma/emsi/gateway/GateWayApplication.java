07fa51a46d6890407dccd83dc493e46b88294d2a
Elhekkioui Dounia <elhekkiouidounia2@gmail.com>
2024-11-13 11:42:29 +0100

    first commit

---
+++ GateWay/src/main/java/ma/emsi/gateway/GateWayApplication.java
@@ -0,0 +1,32 @@
+package ma.emsi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
+    }
+    //@Bean
+    /*public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/users/**").uri("lb/USERMS"))
+                .route(p -> p.path("/organisations/**").uri("lb/ORGANISATIONMS"))
+                .build();
+    }
+
+     */
+}
